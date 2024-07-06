400e044dad758b155055468ebd669c62e9aa2db7
abd-haris <mybestcore19@gmail.com>
2024-07-06 21:01:38 +0700

    mengubah index html dan menghapus js navbar-fixed

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,3 @@
-// navbar fixed
-window.onscroll = function () {
-  const header = document.querySelector("header");
-  const fixNav = header.offsetTop;
-
-  if (window.pageYOffset > fixNav) {
-    header.classList.add("navbar-fixed");
-  } else {
-    header.classList.remove("navbar-fixed");
-  }
-};
-
 // hamburger active
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
