6f410596045bc56f92ee3e18b28c1449ff1ca819
abd-haris <mybestcore19@gmail.com>
2024-07-07 20:54:20 +0700

    mengubah index html

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,17 @@
 // window onscroll
-window.onscroll = function () {
-  const header = document.querySelector("header");
-  const fixNav = header.offsetTop;
-  const toTop = document.querySelector("#to-top");
+// window.onscroll = function () {
+//   const header = document.querySelector("header");
+//   const fixNav = header.offsetTop;
+//   const toTop = document.querySelector("#to-top");
 
-  if (window.scrollY > fixNav) {
-    toTop.classList.remove("hidden");
-    toTop.classList.add("flex");
-  } else {
-    toTop.classList.remove("flex");
-    toTop.classList.add("hidden");
-  }
-};
+//   if (window.scrollY > fixNav) {
+//     toTop.classList.remove("hidden");
+//     toTop.classList.add("flex");
+//   } else {
+//     toTop.classList.remove("flex");
+//     toTop.classList.add("hidden");
+//   }
+// };
 
 // hamburger active
 const hamburger = document.querySelector("#hamburger");
