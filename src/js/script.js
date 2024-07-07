113ef84e90970b13bc8af80ecb5cd5ed7a3ed2a4
abd-haris <mybestcore19@gmail.com>
2024-07-07 20:46:50 +0700

    menambah  fitur to top

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
+// window onscroll
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
+
+  if (window.scrollY > fixNav) {
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
+
 // hamburger active
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#nav-menu");
